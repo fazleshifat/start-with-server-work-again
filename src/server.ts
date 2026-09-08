0e247c4ba3955752f070c571a12917734fcb53ef
fazleshifat <mailbox189.shifat@gmail.com>
2026-09-09 02:20:08 +0600

    connected online databse

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
+import dotenv from 'dotenv';
+// Load environment variables immediately before importing modules that rely on them
+dotenv.config();
+
 import express, { type Express, type Request, type Response } from 'express';
 import { Pool } from "pg";
 
 const app: Express = express();
-const port = 5000;
+const port: number = parseInt(process.env.PORT || '5000', 10);
+// Database
+const pool = new Pool({
+    connectionString: process.env.DATABASE_CONNECTIONSTRING
+})
 
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW(),
+        )
+        `)
+}
 
+initDB();
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World! This is shifat');
