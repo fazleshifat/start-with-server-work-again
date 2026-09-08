3aa37bf989a0aaa06ec428fd1e8f6db49a7ce5e8
fazleshifat <mailbox189.shifat@gmail.com>
2026-09-09 02:13:08 +0600

    add post route with raw status code

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,14 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World! This is shifat');
 });
 
+app.post('/', (req: Request, res: Response) => {
+    console.log(req);
 
+    res.status(201).json({
+        success: true,
+        message: "API is working"
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
